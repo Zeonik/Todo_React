a32218a0fc70d74113066ab8fe99a02ff88ce26a
Ivan Yakovenko <19zeon88@gmail.com>
2019-06-13 22:01:33 +0300

    Adding element

---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './item-add-form.css';
+
+export default class ItemAddForm extends Component {
+    
+    render() {
+        const { onItemAdd } = this.props;
+        return (
+            <div className="item-add-form">
+                <button
+                    className="btn btn-outline-secondary"
+                    onClick={()=>onItemAdd('Hello World')}>
+                    Add Element
+                </button>
+            </div>
+        )
+    }
+}
+
